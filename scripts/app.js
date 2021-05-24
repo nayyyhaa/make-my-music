7df23a572782fac740e64b76cc671c42290c91bf
nayyyhaa <nehaguptang.ng@gmail.com>
2021-05-24 18:17:42 +0530

    feat: mute option

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,7 @@ class MusicTable {
         this.clapAudio = document.querySelector(".clap-sound")
         this.hihatAudio = document.querySelector(".hihat-sound")
         this.kickAudio = document.querySelector(".kick-sound")
+        this.muteBtns = document.querySelectorAll(".mute");
         this.index = 0;
         this.bpm = 150;
         this.isPlaying = null;
@@ -45,17 +46,37 @@ class MusicTable {
         }
     }
     updateBtn() {
-        if (!this.isPlaying) {
+        if (this.isPlaying) {
             this.playBtn.innerText = "Stop";
             this.playBtn.classList.add("active");
         } else {
-            this.playBtn.innerText = "Play";
+            this.playBtn.innerText = "Play!";
             this.playBtn.classList.remove("active");
+        }
+    }
+    muteUnumte(e) {
+        //console.log(typeof (e.target.getAttribute("data-track"))); -"0","1","2" -string
+        let dataTrack = e.target.getAttribute("data-track");
+        e.target.classList.toggle("active");
+        if(e.target.classList.contains("active")){
+            switch(dataTrack) {
+                case "0": this.clapAudio.volume = 0; break;
+                case "1": this.hihatAudio.volume = 0; break;
+                case "2": this.kickAudio.volume = 0; break;
+            }
+        } else {
+            switch(dataTrack) {
+                case "0": this.clapAudio.volume = 1; break;
+                case "1": this.hihatAudio.volume = 1; break;
+                case "2": this.kickAudio.volume = 1; break;
+            }
         }
     }
 }
 
 const musicTable = new MusicTable();
+
+//Event Listeners
 
 musicTable.pads.forEach(pad => {
     pad.addEventListener("click", musicTable.activatePad);
@@ -67,4 +88,10 @@ musicTable.pads.forEach(pad => {
 musicTable.playBtn.addEventListener("click", function() {
     musicTable.play();
     musicTable.updateBtn();
+});
+
+musicTable.muteBtns.forEach( muteBtn => {
+    muteBtn.addEventListener("click", function(e) {
+        musicTable.muteUnumte(e);
+    })
 });
